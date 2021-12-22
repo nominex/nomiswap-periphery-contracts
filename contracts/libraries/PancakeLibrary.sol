a75a602406bb4db3fbc6e3fc925e7efd275bcd1c
Vyacheslav <vyacheslav@MBP-Vyacheslav.local>
2021-12-22 18:01:31 +0400

    feat: update pair hash

--- contracts/libraries/PancakeLibrary.sol
+++ contracts/libraries/PancakeLibrary.sol
@@ -21,7 +21,7 @@ library PancakeLibrary {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'd0d4c4cd0848c93cb4fd1f498d7013ee6bfb25783ea21593d5834f5d250ece66' // init code hash
+                hex'169379d7c6e70a77f9a87a65bd97ea1d7a906e980e5245462c56f91d9706f17a' // init code hash
             ))));
     }
 
